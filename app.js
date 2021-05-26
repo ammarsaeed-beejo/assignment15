87eed72f555a88b19192bdce9014167d033461d8
ammarsaeed-beejo <80599563+ammarsaeed-beejo@users.noreply.github.com>
2021-05-26 13:21:16 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//1. change head
+
+var h = document.getElementsByTagName("h1")[0].innerHTML = "main content Here";
+
+// 2.change color
+
+var p = document.getElementsByTagName('p')[0].style.color = '#985AA0'
+
+var p = document.getElementsByTagName('p')[1].style.color = '#985AA0'
+
+var p = document.getElementsByTagName('p')[2].style.color = '#985AA0'
+
+// 3.add 
+
+var add = document.getElementsByClassName('padding-10')[0].innerHTML += " <a href=>Link 7</a>"
+var add = document.getElementsByClassName('padding-10')[0].innerHTML += " <a href=>Link 1</a>"
+
+iv.parentNode.removeChild(div);
+
+
+
+
+
